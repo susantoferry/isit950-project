3bd0365c5372607878afb6f98fc1f02b55f346d3
Alan <acs222@uowmail.edu.au>
2023-05-11 18:13:34 +1000

    Commit

--- backend/tests.py
+++ backend/tests.py
@@ -4,12 +4,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import LiveServerTestCase
 
+
 class TestCaseLogin(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
 
     def test_page_login(self):
+
+        from django.core.management import call_command
+        from multiprocessing import Process
+        from time import sleep
+        
+        def start_server():
+            call_command('runserver')
+
+        server_process = Process(target=start_server)
+        server_process.start()
+
         browser = self.browser
         browser.get('http://127.0.0.1:8000/tasks')
 
@@ -40,16 +52,18 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_search_in_python_org(self):
         driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element(By.NAME, "q")
-        elem.send_keys("pycon")
+        driver.get("http://www.google.com")
+        self.assertIn("Google", driver.title)
+        elem = driver.find_element(By.id, "APjFqb")
+        elem.send_keys("selenium tutorial")
         elem.send_keys(Keys.RETURN)
+        elem = driver.find_element(By.id, "name")
+
         self.assertNotIn("No results found.", driver.page_source)
 
 
-    def tearDown(self):
-        self.driver.close()
+    # def tearDown(self):
+    #     self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()

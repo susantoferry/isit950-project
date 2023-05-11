1da8aa7d60d84638ece5c875585a6d141a1b8633
Alan <acs222@uowmail.edu.au>
2023-05-11 18:03:02 +1000

    Selenium Testing

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from django.contrib.staticfiles.testing import LiveServerTestCase
 
-# Create your tests here.
+class TestCaseLogin(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def test_page_login(self):
+        browser = self.browser
+        browser.get('http://127.0.0.1:8000/tasks')
+
+        #elements
+        test_email = browser.find_element(By.NAME, "email")
+        test_password = self.browser.find_element(By.NAME, "password")
+        submit = self.browser.find_element(By.NAME, "submit")
+
+        assert "ISIT950 Group Project" in self.browser.title
+        # test_email.send_keys('ferry@gmail.com')
+        # test_password.send_keys('password')
+
+        # submit.send_keys(Keys.RETURN)
+        # self.assertEqual(self.browser.status_code, 200)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element(By.NAME, "q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        self.assertNotIn("No results found.", driver.page_source)
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()

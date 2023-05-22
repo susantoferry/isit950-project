52380aae2616cb60b907933d8632b9e37307c129
Alan Christy Soewargo <70423698+Alancs92@users.noreply.github.com>
2023-05-22 23:17:46 +1000

    Mapbox Update

--- frontend/templatetags/mapbox_stuff.py
+++ frontend/templatetags/mapbox_stuff.py
@@ -8,5 +8,7 @@ locationA = (-33.8536, 151.1542)
 locationB = (-34.424394, 150.89385)
 distance = geodesic(locationA, locationB).kilometers
 
+
+
 print(f"The distance is approximately {distance} kilometers.")
 # RESULT: The distance is approximately 67.71589263256112 kilometers.
